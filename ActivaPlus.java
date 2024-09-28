1281b9484b827d6160f368ae2ce657e697e83790
Preeti Chouhan <89197259+preetichouhan@users.noreply.github.com>
2024-09-28 14:08:00 +0530

    Add files via upload

---
+++ ActivaPlus.java
@@ -0,0 +1,13 @@
+package com.oops;
+
+public class ActivaPlus extends Activa 
+{
+ActivaPlus()
+{
+	System.out.println("I have petrol engine and self start feature");
+}
+public void spped()
+{
+	System.out.println("200");
+}
+}
